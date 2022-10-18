cb85dd78adc31acd768557afb94c9a1234ac62ae
Juanes-xd <juanestebanortizbejarano@gmail.com>
2022-10-18 18:57:07 -0500

    Commit Variable

---
+++ initial.js
@@ -0,0 +1,16 @@
+let nombre = "Juan";
+
+nombre = "Fredy";
+
+const edad = 20;
+
+var apellido = "Ortiz";
+console.log(apellido);
+if (true) {
+  const edad = 30;
+  console.log(edad);
+  console.log(apellido);
+}
+
+console.log(nombre);
+console.log(edad);
